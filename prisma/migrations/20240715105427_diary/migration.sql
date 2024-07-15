c7a985bd8782d2ceff057acf662a9e897044f674
natsukikokubu <natsukikokubu3@gmail.com>
2024-07-15 20:08:41 +0900

    add prisma

---
+++ prisma/migrations/20240715105427_diary/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "diary" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "diary_pkey" PRIMARY KEY ("id")
+);
